37dff769b92bdd84d2992a5200d73c5021d7d6ac
Steven Schrembeck <steven.schrembeck@coxautoinc.com>
2022-04-19 13:31:39 -0400

    linting

--- src/components/Dashboard/StakeAndBurn.jsx
+++ src/components/Dashboard/StakeAndBurn.jsx
@@ -13,32 +13,53 @@ function StakeAndBurn(props) {
   const { contract } = props;
   const { Moralis, account, isAuthenticated } = useMoralis();
   const { isInitialized, methods } = useToken({ contract });
-  const { checkThenAllowFrToken, checkThenAllowFrz, /*checkThenAllowWrapped */} = methods;
+  const {
+    checkThenAllowFrToken,
+    checkThenAllowFrz /*checkThenAllowWrapped */,
+  } = methods;
   const [isTransacting, setIsTransacting] = useState(false);
 
-  if (!isInitialized || !props.address && (!account || !isAuthenticated)) return <Skeleton />;
+  if (!isInitialized || (!props.address && (!account || !isAuthenticated)))
+    return <Skeleton />;
 
-  const genericAmountTransaction = async ({ contractName, functionName, approvals=[], amount }) => {
-    if(!amount) {
+  const genericAmountTransaction = async ({
+    contractName,
+    functionName,
+    approvals = [],
+    amount,
+  }) => {
+    if (!amount) {
       throw new Error(`${amount} is not a valid amount.`);
     }
-    
-    return genericTransaction({ contractName, functionName, approvals, params: {amount}});
-  }
 
-  const genericTransaction = async ({ contractName, functionName, approvals=[], params}) => {
+    return genericTransaction({
+      contractName,
+      functionName,
+      approvals,
+      params: { amount },
+    });
+  };
+
+  const genericTransaction = async ({
+    contractName,
+    functionName,
+    approvals = [],
+    params,
+  }) => {
     setIsTransacting(true);
     try {
-      await Promise.all(approvals.map((approvalFunction) => (
-        approvalFunction({ spender: contract[contractName].address}) // spender is assumed to be the contract we're interacting with
-      )));
+      await Promise.all(
+        approvals.map((approvalFunction) =>
+          approvalFunction({ spender: contract[contractName].address }), // spender is assumed to be the contract we're interacting with
+        ),
+      );
       let options = {
         contractAddress: contract[contractName].address,
         functionName: functionName,
         abi: contract[contractName].abi,
       };
-      if(params) {
-        options.params = {...params};
+      if (params) {
+        options.params = { ...params };
       }
       const transaction = await Moralis.executeFunction(options);
       const confirmation = await transaction.wait();
@@ -46,43 +67,72 @@ function StakeAndBurn(props) {
     } finally {
       setIsTransacting(false);
     }
-  }
+  };
 
-  const burnFrToken = async (amount) => await genericAmountTransaction({ contractName: 'frTokenStaking', functionName: 'stake', approvals: [checkThenAllowFrToken], amount});
-  const stakeFrz = async (amount) => await genericAmountTransaction({ contractName: 'MultiRewards', functionName: 'stake', approvals: [checkThenAllowFrz], amount});
-  const withdrawFrz = async (amount) => await genericAmountTransaction({ contractName: 'MultiRewards', functionName: 'withdraw', amount});
-  const claimAllRewards = async () => await genericTransaction({ contractName: 'MultiRewards', functionName: 'getReward'}); 
+  const burnFrToken = async (amount) =>
+    await genericAmountTransaction({
+      contractName: "frTokenStaking",
+      functionName: "stake",
+      approvals: [checkThenAllowFrToken],
+      amount,
+    });
+  const stakeFrz = async (amount) =>
+    await genericAmountTransaction({
+      contractName: "MultiRewards",
+      functionName: "stake",
+      approvals: [checkThenAllowFrz],
+      amount,
+    });
+  const withdrawFrz = async (amount) =>
+    await genericAmountTransaction({
+      contractName: "MultiRewards",
+      functionName: "withdraw",
+      amount,
+    });
+  const claimAllRewards = async () =>
+    await genericTransaction({
+      contractName: "MultiRewards",
+      functionName: "getReward",
+    });
 
   function Burn() {
     return (
       <div>
         {/* TODO Un-hardcode the burn amount */}
-        <Button loading={isTransacting} onClick={() => burnFrToken(100)}>burn frToken</Button>
+        <Button loading={isTransacting} onClick={() => burnFrToken(100)}>
+          burn frToken
+        </Button>
       </div>
     );
-  };
+  }
   function Stake() {
     return (
       <div>
         {/* TODO Un-hardcode the stake amounts */}
-        <Button loading={isTransacting} onClick={() => stakeFrz(100)}>stake FRZ</Button>
-        <Button loading={isTransacting} onClick={() => withdrawFrz(100)}>withdraw FRZ</Button>
+        <Button loading={isTransacting} onClick={() => stakeFrz(100)}>
+          stake FRZ
+        </Button>
+        <Button loading={isTransacting} onClick={() => withdrawFrz(100)}>
+          withdraw FRZ
+        </Button>
       </div>
     );
-  };
+  }
   function ClaimRewards() {
     return (
       <div>
-        <Button loading={isTransacting} onClick={claimAllRewards}>claim all</Button>
+        <Button loading={isTransacting} onClick={claimAllRewards}>
+          claim all
+        </Button>
       </div>
     );
-  };
-
+  }
 
   return (
     <div>
       <Burn />
-      <Stake />{/* TODO split this into tabs */}
+      <Stake />
+      {/* TODO split this into tabs */}
       <ClaimRewards />
     </div>
   );
