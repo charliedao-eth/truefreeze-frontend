3d9e50ab83bc863edd1d79068bddf77f1b87021b
Steven Schrembeck <steven.schrembeck@coxautoinc.com>
2022-06-14 12:08:05 -0400

    always prod

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.scss";
 
 const APP_ID = process?.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process?.env.REACT_APP_MORALIS_SERVER_URL;
-const IS_PRODUCTION_MODE = process?.env.NODE_ENV !== "development";
+const IS_PRODUCTION_MODE = true;
 
 const Application = () => {
   const isServerInfoConfigured = APP_ID && SERVER_URL ? true : false;
